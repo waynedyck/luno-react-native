2cbe63634497ffb6636523917186964d4df04c4c
Wayne Dyck <dyckw@wsdot.wa.gov>
2017-12-22 12:48:43 -0800

    Updated dependencies and app starts with warnings

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.946"]
-                 [reagent "0.8.0-alpha2" :exclusions [cljsjs/react cljsjs/react-dom cljsjs/react-dom-server cljsjs/create-react-class]]
-                 [re-frame "0.10.3-alpha2"]
+                 [reagent "0.7.0" :exclusions [cljsjs/react cljsjs/react-dom cljsjs/react-dom-server cljsjs/create-react-class]]
+                 [re-frame "0.10.2"]
                  [prismatic/schema "1.1.7"]
                  [cljs-ajax "0.7.3"]]
   :plugins [[lein-cljsbuild "1.1.7"]
